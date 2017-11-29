761192b01dd9103dc00ddc270d0da456c55bb01f
elbandi <bandispam@gmail.com>
2017-11-29 02:20:28 +0100

    Balance response use quoted value

--- balance.go
+++ balance.go
@@ -2,6 +2,6 @@ package hitbtc
 
 type Balance struct {
 	Currency  string  `json:"currency"`
-	Available float64 `json:"available"`
-	Reserved  float64 `json:"reserved"`
+	Available float64 `json:"available,string"`
+	Reserved  float64 `json:"reserved,string"`
 }
